a271940a60ac3f54aedf922d04ca2d34ccf9b460
Taha Yeşilyurt <taha.yesilyurt61@gmail.com>
2025-12-14 00:51:36 +0300

    Fix5

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -2,40 +2,34 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 // GitHub girişi için gerekli olan 'GithubAuthProvider' ve 'signInWithPopup' eklendi
-import { 
-    getAuth, 
-    createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword, 
-    signOut, 
-    onAuthStateChanged, 
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
     updateProfile,
     GithubAuthProvider, // EKLENDI
     signInWithPopup     // EKLENDI
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-import { 
-    getFirestore, 
-    collection, 
-    addDoc, 
-    getDocs, 
-    doc, 
-    deleteDoc, 
-    updateDoc, 
-    query, 
-    where, 
-    getDoc, 
-    setDoc 
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    doc,
+    deleteDoc,
+    updateDoc,
+    query,
+    where,
+    getDoc,
+    setDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // --- SENIN CONFIG BILGILERIN ---
-const firebaseConfig = {
-  apiKey: "AIzaSyAiQBvNTcD1M9SzU04dLXwp2ayFvZ3trZg",
-  authDomain: "quel-400b2.firebaseapp.com",
-  projectId: "quel-400b2",
-  storageBucket: "quel-400b2.firebasestorage.app",
-  messagingSenderId: "544893245562",
-  appId: "1:544893245562:web:1922777c9d813feafe83c3"
-};
+// .gitignore ile gizlenmiş config.js dosyasından alıyoruz
+import firebaseConfig from './config.js';
 
 // Firebase'i başlat
 const app = initializeApp(firebaseConfig);
@@ -46,23 +40,23 @@ const db = getFirestore(app);
 const provider = new GithubAuthProvider();
 
 // Dışarı Aktar (Export)
-export { 
-    auth, 
-    db, 
+export {
+    auth,
+    db,
     provider, // ARTIK DISARI AKTARILIYOR
     signInWithPopup, // ARTIK DISARI AKTARILIYOR
-    createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword, 
-    signOut, 
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
     onAuthStateChanged,
     updateProfile,
-    collection, 
-    addDoc, 
-    getDocs, 
-    doc, 
-    deleteDoc, 
-    updateDoc, 
-    query, 
+    collection,
+    addDoc,
+    getDocs,
+    doc,
+    deleteDoc,
+    updateDoc,
+    query,
     where,
     getDoc,
     setDoc
